96a5f4d376a9a39f41279bf5aa93009e75310828
Francisco Veiras <74626997+francisco-svg761@users.noreply.github.com>
2025-03-08 12:57:14 -0300

    feat: signIn

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -2,8 +2,24 @@ import { Hono } from 'hono';
 import { cors } from 'hono/cors';
 import { pool } from './db';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 const app = new Hono();
+
+const ACCESS_EXPIRES_IN = '2h';
+const REFRESH_EXPIRES_IN = '7d';
+
+const generateTokens = (userId: string) => {
+  const accessToken = jwt.sign({ userId }, process.env.JWT_SECRET!, {
+    expiresIn: ACCESS_EXPIRES_IN,
+  });
+
+  const refreshToken = jwt.sign({ userId }, process.env.REFRESH_SECRET!, {
+    expiresIn: REFRESH_EXPIRES_IN,
+  });
+
+  return { accessToken, refreshToken };
+};
 
 app.get('/', (c) => c.text('Hono!'));
 
@@ -42,7 +58,41 @@ app.post('/auth/signUp', async (c) => {
 });
 
 app.post('/auth/signIn', async (c) => {
-  return c.json({ message: 'next step' }, { status: 200 });
+  const { email, password } = await c.req.json();
+
+  if (!email || !password) {
+    return c.json({ error: 'Email and password required' }, 400);
+  }
+
+  try {
+    const result = await pool.query(
+      'SELECT * FROM auth.users WHERE email = $1 LIMIT 1',
+      [email]
+    );
+
+    if (result.rows.length) {
+      const user = result.rows[0];
+
+      const isMatch = await bcrypt.compare(password, user.password_hash);
+
+      if (!isMatch)
+        return c.json({ message: 'Invalid credentials' }, { status: 401 });
+
+      const { accessToken, refreshToken } = generateTokens(user.id);
+
+      await pool.query(
+        'UPDATE auth.users SET refresh_token = $1 WHERE id = $2',
+        [refreshToken, user.id]
+      );
+
+      return c.json({ accessToken }, { status: 201 });
+    } else {
+      return c.json({ message: 'Invalid credentials' }, { status: 401 });
+    }
+  } catch (error) {
+    console.error(error);
+    return c.json({ error: 'Error logging in' });
+  }
 });
 
 export default {
--- apps/website/app/routes/home.tsx
+++ apps/website/app/routes/home.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import type { Route } from './+types/home';
 
 export function meta({}: Route.MetaArgs) {
@@ -8,8 +9,20 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const [access, setAccess] = useState<'signIn' | 'signUp'>('signIn');
+
   function signUp(formData: FormData) {
     fetch(`${import.meta.env.VITE_BACKEND_URL!}/auth/signUp`, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: formData.get('email'),
+        password: formData.get('password'),
+      }),
+    });
+  }
+
+  function signIn(formData: FormData) {
+    fetch(`${import.meta.env.VITE_BACKEND_URL!}/auth/signIn`, {
       method: 'POST',
       body: JSON.stringify({
         email: formData.get('email'),
@@ -22,25 +35,54 @@ export default function Home() {
     <div className="h-[100dvh] w-screen overflow-hidden bg-gray-200">
       <div className="w-full flex-col gap-4 h-full items-center justify-center flex">
         <h1 className="text-black">OAuth example</h1>
-        <form action={signUp} className="flex flex-col gap-4 min-w-1/4">
-          <input
-            required
-            autoComplete="off"
-            name="email"
-            type="email"
-            className="bg-gray-400 rounded-lg shadow-2xl py-1 px-2"
-          />
-          <input
-            required
-            autoComplete="off"
-            name="password"
-            type="password"
-            className="bg-gray-400 rounded-lg shadow-2xl py-1 px-2"
-          />
-          <button className="bg-green-300 hover:bg-green-400 transition-all py-1 rounded-lg text-black cursor-pointer">
-            Sign up
-          </button>
-        </form>
+        {access === 'signIn' ? (
+          <form action={signIn} className="flex flex-col gap-4 min-w-1/4">
+            <input
+              required
+              autoComplete="off"
+              name="email"
+              type="email"
+              className="bg-gray-400 rounded-lg shadow-2xl py-1 px-2"
+            />
+            <input
+              required
+              autoComplete="off"
+              name="password"
+              type="password"
+              className="bg-gray-400 rounded-lg shadow-2xl py-1 px-2"
+            />
+            <button className="bg-green-300 hover:bg-green-400 transition-all py-1 rounded-lg text-black cursor-pointer">
+              Sign in
+            </button>
+
+            <div onClick={() => setAccess('signUp')} className="cursor-pointer">
+              <p>Don't have an account? Sign Up</p>
+            </div>
+          </form>
+        ) : (
+          <form action={signUp} className="flex flex-col gap-4 min-w-1/4">
+            <input
+              required
+              autoComplete="off"
+              name="email"
+              type="email"
+              className="bg-gray-400 rounded-lg shadow-2xl py-1 px-2"
+            />
+            <input
+              required
+              autoComplete="off"
+              name="password"
+              type="password"
+              className="bg-gray-400 rounded-lg shadow-2xl py-1 px-2"
+            />
+            <button className="bg-green-300 hover:bg-green-400 transition-all py-1 rounded-lg text-black cursor-pointer">
+              Sign up
+            </button>
+            <div onClick={() => setAccess('signIn')} className="cursor-pointer">
+              <p>Already have an account? Sign In</p>
+            </div>
+          </form>
+        )}
       </div>
     </div>
   );
